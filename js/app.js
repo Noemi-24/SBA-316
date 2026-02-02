4538893f2e88f5cd2eb5d80b845889301d2c0b15
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-02 16:53:30 -0500

    Add DOM functionality

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// TO-DO List Manager - SBA 316
+// ================================
+
+
+//========== DOM Elements ==========
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.querySelector('#taskInput');
+const taskList = document.getElementById('taskList');
+const taskCount = document.querySelector('#taskCount');
+
